17962f7976a2b29b306d1e6681d5d904db287fc6
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-09 21:01:27 +0000

    Add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  enum role: %i[user driver admin]
+  after_initialize :set_default_role, if: :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+
+
 end
