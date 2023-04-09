c329be10387b902f4408092ec284a0f2e8646c2a
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-09 20:31:21 +0000

    Add authentication routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  layout :set_layout
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
+
+  def set_layout
+    if current_user
+      'application'
+    else
+      'auth'
+    end
+  end
+
+
 end
