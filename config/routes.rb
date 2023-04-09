887ecbaa8edef6d6d2e92d8fa954bbc317571084
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-09 21:27:16 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get '/about', to: 'homes#about', as: 'about_me'
-
-  resources :languages do
-    resources :courses do 
-        end
-  end
-
-  # get '/languages', to: 'languages#index', as: 'languages'
-  # get '/languages/new', to: 'languages#new', as: 'new_language'
-  # post '/languages', to: 'languages#create', as: 'create_language'
-  # get '/languages/:id', to: 'languages#show', as: 'language'
-  # get '/about', to: 'homes#about', as: 'about'
-  # get '/languages/:id/edit', to: 'languages#edit', as: 'edit_language'
-  # patch '/languages/:id', to: 'languages#update', as: 'update_language'
-  # delete '/languages/:id', to: 'languages#destroy', as: 'destroy_language'
+  # Home page
   root "homes#index"
+
+  # About page
+  get '/about', to: 'homes#about', as: 'about'
+
+  # User authentication
+  devise_for :users, path: 'auth', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+    registration: 'register',
+    sign_up: 'signup'
+  }
+
+  get '/driver-dashboard', to: 'driver_dashboard#index', as: 'driver_dashboard'
+  get '/admin-dashboard', to: 'admin_dashboard#index', as: 'admin_dashboard'
 end
