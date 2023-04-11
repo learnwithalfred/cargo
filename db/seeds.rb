b5679feecf658e9e0b6caf10a834117b83767321
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-11 15:32:19 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,19 @@
-10.times do
+
+  User.create!(
+    email: "admin@example.com",
+    password: "secrete",
+    password_confirmation: "secrete",
+    confirmed_at: DateTime.now,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    role: 'admin',
+    address: Faker::Address.full_address,
+    phone: Faker::PhoneNumber.cell_phone,
+    gender: "male"
+  )
+
+
+30.times do
   User.create!(
     email: Faker::Internet.email,
     password: "secrete",
@@ -6,11 +21,14 @@
     confirmed_at: DateTime.now,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    role: Faker::Number.between(from: 1, to: 2),
+    role: Faker::Number.between(from: 1, to: 4),
+    address: Faker::Address.full_address,
+    phone: Faker::PhoneNumber.cell_phone,
+    gender: "male"
   )
 end
 
-10.times do
+50.times do
   Gp.create!(
     container_no: Faker::Alphanumeric.alphanumeric(number: 11),
     weight: Faker::Number.between(from: 2000, to: 40000),
@@ -23,7 +41,7 @@ end
     line: Faker::Company.name,
     size: Faker::Lorem.word,
     depot: Faker::Address.community,
-    sheet: Faker::Alphanumeric.alphanumeric(number: 8),
-    creator: User.all.sample
+    sheet: User.where(role:"staff").sample.id,
+    creator: User.where(role:"staff").sample
   )
 end
