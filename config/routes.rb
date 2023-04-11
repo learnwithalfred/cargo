ecb675074022dba94a747b30824daeb0eb6b3be5
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-11 10:31:43 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,8 @@ Rails.application.routes.draw do
   }
 
   get '/driver-dashboard', to: 'driver_dashboard#index', as: 'driver_dashboard'
-  get '/admin-dashboard', to: 'admin_dashboard#index', as: 'admin_dashboard'
+  get '/admin', to: 'admin_dashboard#index', as: 'admin_dashboard'
+  get '/admin/user-list', to: 'admin_dashboard#user_list', as: 'user_list'
+  get '/admin/edit-user/:id', to: 'admin_dashboard#edit_user', as: 'edit_user'
+  patch '/admin/update-user/:id', to: 'admin_dashboard#update', as: 'update_user'
 end
